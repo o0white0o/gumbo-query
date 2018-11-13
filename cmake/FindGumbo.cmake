38e69bab456286e92b29eff2348260eea179150d
Aleksei Drozdovskii <aldrozdovskij@gmail.com>
2018-11-13 20:16:25 +0300

    Update cmake

--- cmake/FindGumbo.cmake
+++ cmake/FindGumbo.cmake
@@ -8,22 +8,23 @@
 #  Gumbo_LIBRARIES - libraries
 
 
-
 # Include dir
 find_path(Gumbo_INCLUDE_DIRS
-  NAMES gumbo/gumbo.h
-)
+        NAMES gumbo/gumbo.h
+        )
 
 # Finally the library itself
 find_library(Gumbo_LIBRARY
-  NAMES libgumbo.dylib libgumbo.dll gumbo.dylib gumbo.dll libgumbo.so
-)
+        NAMES libgumbo.dylib libgumbo.dll gumbo.dylib gumbo.dll libgumbo.so
+        )
 
 find_library(Gumbo_static_LIBRARY
-  NAMES libgumbo.a libgumbo_static.a gumbo.a gumbo_static.a libgumbo.lib libgumbo_static.lib gumbo.lib gumbo_static.lib
-)
+        NAMES libgumbo.a libgumbo_static.a gumbo.a gumbo_static.a libgumbo.lib libgumbo_static.lib gumbo.lib gumbo_static.lib
+        )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
 set(Gumbo_PROCESS_INCLUDES Gumbo_INCLUDE_DIR)
 set(Gumbo_PROCESS_LIBS Gumbo_LIBRARY Gumbo_static_LIBRARY)
+
+
